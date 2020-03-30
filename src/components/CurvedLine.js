cca3b6ea0693fb556c14b2c321bf4a36a3e5149b
RafaeloDuarte <rafaeloduarte@gmail.com>
2020-03-30 14:16:20 -0300

    Axes and Scales

--- src/components/CurvedLine.js
+++ src/components/CurvedLine.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { select, line, curveCardinal } from 'd3';
+import { select, line, curveCardinal, axisBottom, axisRight, scaleLinear } from 'd3';
 
 function CurvedLine() {
 
@@ -8,15 +8,33 @@ function CurvedLine() {
 
     useEffect(() => {
         const svg = select(svgRef.current)
+        const xScale = scaleLinear()
+            .domain([0, data.length - 1])
+            .range([0, 300])
+
+        const yScale = scaleLinear()
+            .domain([0, 150])
+            .range([150, 0])
+        const xAxis = axisBottom(xScale).ticks(data.length)
+        svg.select(".x-axis")
+            .style("transform", "translateY(150px)")
+            .call(xAxis)
+
+        const yAxis = axisRight(yScale)
+        svg.select(".y-axis")
+            .style("transform", "translateX(300px)")
+            .call(yAxis)
+
         const myLine = line()
-            .x((value, index) => index * 50)
-            .y(value => 150 - value)
+            .x((value, index) => xScale(index))
+            .y(yScale)
             .curve(curveCardinal)
-        svg
-            .selectAll("path")
+
+        svg.selectAll(".line")
             .data([data])
             .join("path")
-            .attr("d", value => myLine(value))
+            .attr("class", "line")
+            .attr("d", myLine)
             .attr("fill", "none")
             .attr("stroke", "blue")
 
@@ -25,9 +43,10 @@ function CurvedLine() {
     return (
         <>
             <svg ref={svgRef}>
-                <path ></path>
+                <g className="x-axis" />
+                <g className="y-axis" />
             </svg>
-            <br />
+            <br /><br />
             <button onClick={() => setData(data.map(value => value + 5))}>Update Data</button>
             <button onClick={() => setData(data.filter(value => value <= 35))}>Filter Data</button>
 
